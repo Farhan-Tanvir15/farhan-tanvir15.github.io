e5774d654438f0c8c0faeb02ff1b01bdf9e279c1
Farhan Tanvir Rizve <71425272+Farhan-Tanvir15@users.noreply.github.com>
2021-05-27 12:39:23 +0600

    nav, favicon & back to top

--- main.js
+++ main.js
@@ -25,3 +25,23 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 6000); // Change image every 6 seconds
 }
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
